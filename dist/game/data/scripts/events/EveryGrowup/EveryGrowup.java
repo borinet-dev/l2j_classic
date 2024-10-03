a493b70d4e351916c0107652ee349c85e42b20f0
가츠 <master@borinet.dev>
2024-10-03 16:50:31 +0900

    모두의 성장 이벤트 누락 파일.

---
+++ dist/game/data/scripts/events/EveryGrowup/EveryGrowup.java
@@ -0,0 +1,15 @@
+package events.EveryGrowup;
+
+import org.l2jmobius.gameserver.model.quest.LongTimeEvent;
+
+public class EveryGrowup extends LongTimeEvent
+{
+	public EveryGrowup()
+	{
+	}
+	
+	public static void main(String[] args)
+	{
+		new EveryGrowup();
+	}
+}
