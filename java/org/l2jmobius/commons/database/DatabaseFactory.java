859be1fe98058bce99744df7d4186a25a12659df
Administrator <Administrator@E5-2687w-v2>
2024-12-30 02:08:09 +0900

    mariadb 버전 업데이트.

--- java/org/l2jmobius/commons/database/DatabaseFactory.java
+++ java/org/l2jmobius/commons/database/DatabaseFactory.java
@@ -1,19 +1,3 @@
-/*
- * This file is part of the L2J Mobius project.
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.l2jmobius.commons.database;
 
 import java.sql.Connection;
@@ -24,19 +8,31 @@ import org.mariadb.jdbc.MariaDbPoolDataSource;
 import org.l2jmobius.Config;
 
 /**
- * @author Mobius
- * @version November 10th 2018
+ * @author 보리넷 가츠
+ * @version December 26th 2024
  */
 public class DatabaseFactory
 {
 	private static final Logger LOGGER = Logger.getLogger(DatabaseFactory.class.getName());
 	
-	private static final MariaDbPoolDataSource DATABASE_POOL = new MariaDbPoolDataSource(Config.DATABASE_URL + "&user=" + Config.DATABASE_LOGIN + "&password=" + Config.DATABASE_PASSWORD + "&maxPoolSize=" + Config.DATABASE_MAX_CONNECTIONS);
-	private static final MariaDbPoolDataSource DATABASE_LOG = new MariaDbPoolDataSource(Config.DATABASE_LOG_URL + "&user=" + Config.DATABASE_LOGIN + "&password=" + Config.DATABASE_PASSWORD + "&maxPoolSize=" + Config.DATABASE_MAX_CONNECTIONS);
+	private static final MariaDbPoolDataSource DATABASE_POOL;
+	private static final MariaDbPoolDataSource DATABASE_LOG;
+	
+	static
+	{
+		try
+		{
+			DATABASE_POOL = new MariaDbPoolDataSource(Config.DATABASE_URL + "&user=" + Config.DATABASE_LOGIN + "&password=" + Config.DATABASE_PASSWORD + "&maxPoolSize=" + Config.DATABASE_MAX_CONNECTIONS);
+			DATABASE_LOG = new MariaDbPoolDataSource(Config.DATABASE_LOG_URL + "&user=" + Config.DATABASE_LOGIN + "&password=" + Config.DATABASE_PASSWORD + "&maxPoolSize=" + Config.DATABASE_MAX_CONNECTIONS);
+		}
+		catch (Exception e) // SQLException 대신 Exception 사용
+		{
+			throw new RuntimeException("Failed to initialize MariaDbPoolDataSource", e);
+		}
+	}
 	
 	public static void init()
 	{
-		// Test if connection is valid.
 		try
 		{
 			DATABASE_POOL.getConnection().close();
